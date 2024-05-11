23b972a8f7ef940125e850c4da16c292a1402b8f
MasterGar1 <hristovgavril296@gmail.com>
2024-05-11 23:29:54 +0300

    Wrench fix

--- enemy/enemy.gd
+++ enemy/enemy.gd
@@ -1,32 +1,26 @@
 extends CharacterBody2D
 
 @onready var area = $Area
-@onready var attack_rot = $AttackRotation
+@onready var animation_player = $AnimationPlayer
 
 const JUPM_STRENGTH = 50
 const GRAVITY = 3
 
 enum {
-	IDLE, ATTACK
+	IDLE, ATTACK, STATIC
 }
 
 var state : int = IDLE
 
-func _physics_process(delta):
+func _process(delta):
 	if area.can_see_entity() and state == IDLE:
 		state = ATTACK
-		attack_rot.get_child(0).position.x = area.entity.position.distance_to(position)
-		
-		var direction = global_position.direction_to(area.entity.global_position)
-		
-		attack_rot.rotation = direction.angle()
-		if direction.angle() <= PI / 2 and direction.angle() >= -PI/2:  
-			velocity = direction.rotated(-PI/4) * JUPM_STRENGTH
-		else:
-			velocity = direction.rotated(PI/4) * JUPM_STRENGTH
+		animation_player.play("attack")
 			
 	if velocity == Vector2.ZERO:
 		state = IDLE
-			
-	velocity = velocity * delta * GRAVITY
-	move_and_collide(velocity)
+
+
+func _on_animation_finished(anim_name):
+	if anim_name == "attack":
+		pass
