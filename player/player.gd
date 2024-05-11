0f83d21318d9375ad458d84ef6205d230c48973e
Viktor Pazhev <asusvikp@gmail.com>
2024-05-11 14:27:12 +0300

    Set wrench position when held

--- player/player.gd
+++ player/player.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
+@onready var sprite = $Sprite2D
+
 var state : int = MOVE
 
 #States
@@ -27,4 +29,12 @@ func _physics_process(delta):
 func move(delta):
 	var input_direction : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED * delta
+	
+	var hold_pos = find_child("HoldPosition")
+	if hold_pos != null && input_direction != Vector2.ZERO:
+		var offset = Vector2(0, -20)
+		hold_pos.look_at(position)
+		#hold_pos.set_rotatd
+		hold_pos.set_position(input_direction.normalized() * 40)
+	
 	move_and_collide(velocity)
--- player/wrench.gd
+++ player/wrench.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,6 +11,12 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	if body is CharacterBody2D:
-		call_deferred("reparent", body)
+	var hold_pos = body.find_child("HoldPosition")
+	
+	if hold_pos != null && hold_pos.get_children().size() == 0 && body is CharacterBody2D:
+		call_deferred("get_picked_up", hold_pos)
 		print("Picked up!")
+
+func get_picked_up(body) :
+	reparent(body)
+	position = Vector2.ZERO
