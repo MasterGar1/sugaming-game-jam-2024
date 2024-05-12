f013ab9e7911db1eee56975a28da23a0cd1a552c
Viktor Pazhev <asusvikp@gmail.com>
2024-05-12 15:30:48 +0300

    Fix generator room bug

--- rooms/generator_room.gd
+++ rooms/generator_room.gd
@@ -5,6 +5,7 @@ var integral_index : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	super()
 	integral_index = rng.randi_range(1, 7)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
